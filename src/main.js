5afe3582100b4ca7a1cd517f1d7c051a849ffe44
Thomas Huchedé <huchede.thomas@gmail.com>
2020-04-15 21:39:53 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import App from './App.svelte';
+import { makeServer } from './server';
+
+
+makeServer();
+
+const app = new App({
+	target: document.body,
+	props: {
+	}
+});
+
+export default app;
